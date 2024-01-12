0f9d4a27a2ff456349f5e7b50f2bd4a7f730661a
asselzhl <asselzhly@gmail.com>
2024-01-12 17:25:24 +0600

    change opacity of side menu

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -9,20 +9,20 @@ import { MdOutlineWork, MdLocalPhone } from "react-icons/md";
 
 const SideMenu = () => {
   return (
-    <div className='flex flex-col gap-y-12 fixed left-0 top-[7%] py-20 px-4 bg-transparent-gray backdrop-blur-md z-20 rounded-r-[30px]'>
-        <div className='p-[14px] bg-gray-300 rounded-[50%]'>
+    <div className='flex flex-col gap-y-12 fixed left-0 top-[7%] py-20 px-4 bg-transparent-gray opacity-50 hover:opacity-100 ease-in-out duration-300 backdrop-blur-md z-20 rounded-r-[30px]'>
+        <div className='p-[14px] hover:cursor-pointer bg-gray-300 rounded-[50%]'>
             <FiHome color='white' size={24} />
         </div>
-        <div className='p-[14px]'>
+        <div className='p-[14px] hover:cursor-pointer'>
             <FiUser color='#0F103F' size={24} />
         </div>
-        <div className='p-[14px]'>
+        <div className='p-[14px] hover:cursor-pointer'>
             <HiSquare3Stack3D color='#0F103F' size={24} />
         </div>
-        <div className='p-[14px]'>
+        <div className='p-[14px] hover:cursor-pointer'>
             <MdOutlineWork color='#0F103F' size={24} />
         </div>
-        <div className='p-[14px]'>
+        <div className='p-[14px] hover:cursor-pointer'>
             <MdLocalPhone color='#0F103F' size={24} />
         </div>
     </div>
