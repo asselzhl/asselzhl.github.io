89c3f11fe618c086aa74ebc312381292358f00be
asselzhl <asselzhly@gmail.com>
2024-01-15 13:00:08 +0600

    implement scroll to top button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { TypeAnimation } from 'react-type-animation';
+import ScrollToTop from "react-scroll-to-top";
 import cv from '../assets/pdf/Assel_Zholaushybay_Frontend_Developer.pdf'
 
 const Home = () => {
@@ -22,6 +23,9 @@ const Home = () => {
             <h1 className='text-white text-3xl sm:text-5xl md:text-6xl font-bold mb-2.5'>Assel Zholaushybay</h1>
             <h2 className='text-2xl sm:text-4xl md:text-5xl text-[#6083FF] font-bold leading-snug mb-8'>I’m a Front-end Developer</h2>
             <a href={cv} target='_blank' className='inline-block bg-gradient-to-r from-[#C961DE] to-[#2954A3] hover:scale-110 ease-in-out duration-300 text-white py-3 px-9 rounded-[30px] border border-transparent-white border-solid'>Download CV</a>
+        </div>
+        <div style={{ marginTop: "150vh" }}>
+          <ScrollToTop smooth color='white' className='scroll-to-top' />
         </div>
       </div>
     </div>
